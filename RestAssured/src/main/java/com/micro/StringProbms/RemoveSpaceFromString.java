70117cd9e879a541b97184b5b2baa66939dddda8
JangamHemaSekharaiah <sekharhema0001@gmail.com>
2026-09-24 11:41:31 +0530

    Updatiing branch

--- RestAssured/src/main/java/com/micro/StringProbms/RemoveSpaceFromString.java
+++ RestAssured/src/main/java/com/micro/StringProbms/RemoveSpaceFromString.java
@@ -2,5 +2,22 @@ package com.micro.StringProbms;
 
 public class RemoveSpaceFromString
 {
+    public static void main(String[] args)
+    {
+        String str = "Java is Powerful";
+
+
+        for (int i = 0 ; i < str.length() ; i++)
+        {
+            if (str.charAt(i)==' ')
+            {
+                continue;
+            }
+            System.out.print(str.charAt(i));
+
+        }
+
+
+    }
 
 }
